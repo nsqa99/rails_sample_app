4cfd2c12ada93fb2ae587d2664310d0494beda34
NSQA <nsqa99@github.com>
2021-07-13 14:40:21 +0700

    implement basic login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,3 @@
 class ApplicationController < ActionController::Base
-    def hello
-        render html: "Running on Rails..."
-    end
+    include SessionsHelper
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,17 @@
 class User < ApplicationRecord
-    before_save { email.downcase! }
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-    validates :name, presence: true, length: {maximum: 50}
-    validates :email, presence: true,
-                    length: {maximum: 255},
-                    format: {with: VALID_EMAIL_REGEX},
-                    uniqueness: true
-    validates :password, length: {minimum:6},
-                        presence: true
-    has_secure_password
+  before_save { email.downcase! }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :name, presence: true, length: {maximum: 50}
+  validates :email, presence: true,
+                  length: {maximum: 255},
+                  format: {with: VALID_EMAIL_REGEX},
+                  uniqueness: true
+  validates :password, length: {minimum:6},
+                      presence: true
+  has_secure_password
+  
+	def self.digest(string)
+		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+		BCrypt::Password.create(string, cost: cost)
+  end
 end
