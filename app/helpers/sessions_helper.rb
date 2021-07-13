48e7ad559f458dcf75af433b27419cad06668e8f
NSQA <nsqa99@github.com>
2021-07-13 17:02:02 +0700

    Implement advanced login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,11 +3,24 @@ module SessionsHelper
     def log_in(user)
 			session[:user_id] = user.id
     end
+
+		#Remember users in a persistent session
+		def remember(user)
+			user.remember
+			cookies.permanent.signed[:user_id] = user.id
+			cookies.permanent[:remember_token] = user.remember_token
+		end
 		
 		# Returns the current logged-in user (if any).
 		def current_user
-			if session[:user_id]
-				@current_user ||= User.find_by(id: session[:user_id])
+			if (user_id = session[:user_id])
+				@current_user ||= User.find_by(id: user_id)
+			elsif (user_id = cookies.signed[:user_id])
+				user = User.find_by(id: user_id)
+				if user&.authenticated?(cookies[:remember_token])
+					log_in user
+					@current_user = user
+				end
 			end
 		end
 
@@ -18,7 +31,15 @@ module SessionsHelper
 
 		#log out
 		def log_out
+			forget(current_user)
 			session.delete(:user_id)
 			@current_user = nil
+		end
+
+		# Forget a permanent session
+		def forget(user)
+			user.forget
+			cookies.delete(:user_id)
+			cookies.delete(:remember_token)
 		end
 end
