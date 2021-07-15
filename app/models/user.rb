e54ec259c6ec336d4b9581159659ce6beb982c1b
NSQA <nsqa99@github.com>
2021-07-15 15:35:04 +0700

    finish following user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,15 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships,
+            class_name: "Relationship", 
+            foreign_key: "follower_id", 
+            dependent: :destroy
+  has_many :passive_relationships,
+            class_name: "Relationship", 
+            foreign_key: "followed_id", 
+            dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
@@ -64,7 +74,20 @@ class User < ApplicationRecord
   end
   # get posts on news feed
   def feed
-    Micropost.where("user_id = ?", id)
+    following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
+  end
+
+  def follow(other)
+    following << other
+  end
+
+  def unfollow(other)
+    following.delete(other)
+  end
+
+  def following?(other)
+    following.include?(other)
   end
   
   private
